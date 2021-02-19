93e3a76235efa2adc5033f522917cc3780fa50de
MingliangChen <cnaylor@163.com>
2021-02-19 18:03:25 +0800

    first commit

---
+++ src/main/java/com/naylor/springboot/avoidrepeat/AvoidRepeatApplication.java
@@ -0,0 +1,13 @@
+package com.naylor.springboot.avoidrepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvoidRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvoidRepeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naylor/springboot/avoidrepeat/AvoidRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naylor.springboot.avoidrepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvoidRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
